83e22b37bc791d92b10373e0c463ab6bf3032089
zhuddan <yobaci@163.com>
2025-01-07 13:12:58 +0800

    fix: 898

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,11 @@
-type Includes<T extends readonly any[], U> = any
+ type IsEqual<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2)
+    ? true
+    : false
+
+type Includes<T extends readonly any[], U> = T extends [infer First, ...infer Rest]
+  ? IsEqual<First, U> extends true
+    ? true
+    : Includes<Rest, U>
+  : false
