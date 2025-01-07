5237bd3486abac2e227b884973f8a55e170745d0
zhuddan <yobaci@163.com>
2025-01-07 18:34:14 +0800

    fix: 116

--- questions/00116-medium-replace/template.ts
+++ questions/00116-medium-replace/template.ts
@@ -1,1 +1,4 @@
-type Replace<S extends string, From extends string, To extends string> = any
+type Replace<S extends string, From extends string, To extends string> =
+  S extends `${infer L}${From extends '' ? never : From}${infer R}`
+    ? `${L}${To}${R}`
+    : S
