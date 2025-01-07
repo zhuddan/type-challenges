0239d20cab763813416f46ff8fca7c83c01929b1
zhuddan <yobaci@163.com>
2025-01-07 10:44:52 +0800

    fix: 189

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,6 @@
-type MyAwaited<T> = any
+type MyAwaited<T> = T extends null | undefined ? T :
+  T extends object & { then: (v: infer F) => any } ?
+    F extends (v: infer V) => any ? MyAwaited<V>
+      : never
+    :
+    T
