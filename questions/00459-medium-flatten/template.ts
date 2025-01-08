917452913ff1aa6963fc9170f6a5ba7ba9a6b9ba
zhuddan <yobaci@163.com>
2025-01-08 10:10:12 +0800

    fix: 459

--- questions/00459-medium-flatten/template.ts
+++ questions/00459-medium-flatten/template.ts
@@ -1,1 +1,6 @@
-type Flatten = any
+type Flatten<U extends any[], T extends any[] = []> =
+U extends [infer X, ...infer Y] ?
+  X extends any[]
+    ? Flatten<[...X, ...Y], T>
+    : Flatten<[...Y], [...T, X ]>
+  : T
