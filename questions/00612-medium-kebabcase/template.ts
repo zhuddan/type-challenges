2e92d0c20fd20fed389af813fc3eb8196ef96260
zhuddan <yobaci@163.com>
2025-01-08 16:20:48 +0800

    fix: 612

--- questions/00612-medium-kebabcase/template.ts
+++ questions/00612-medium-kebabcase/template.ts
@@ -1,1 +1,4 @@
-type KebabCase<S> = any
+type KebabCase<S extends string> =
+ S extends `${infer A}${infer B}`
+   ? B extends Uncapitalize<B> ? `${Uncapitalize<A>}${KebabCase<B>}` : `${Uncapitalize<A>}-${KebabCase<B>}`
+   : S
